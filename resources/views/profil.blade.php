ed8e693e1b135b63b72505369fae4fa30bf83419
Faishal Mumtazah <faishal@students.amikom.ac.id>
2026-04-10 23:51:48 +0700

    Update profil.blade.php

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -436,7 +436,7 @@
                 <a href="/kontak" class="nl">Kontak</a>
             </div>
             <div class="nav-user">
-                <div class="av">MH</div>
+                <div class="av">RID</div>
                 <div>
                     <p style="font-size:12px;font-weight:600;color:var(--text);line-height:1.2;">Ratih Intan Dwyatmasari</p>
                     <p style="font-size:10px;color:var(--muted);">24.12.3271</p>
